752de2ec35892712ca7b2f2a68fc6255726c63dc
Luca Benjamin Frei <lfr111912@iet-gibb.ch>
2020-03-26 10:40:10 +0100

    almost finished AB4 working on last page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_patameters, if: :devise_controller?
-
   protected
 
   def configure_permitted_patameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :website])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
